5c83e2c7cd8ae1eb7baddc7926e1334db70e8ebd
lilkajt <20mojekonto02@gmail.com>
2023-02-25 02:35:50 +0100

    Update v1.2

--- Script/SAG.py
+++ Script/SAG.py
@@ -1,3 +1,7 @@
+# 25.02.2023
+# Version 1.2 - tested only on windows 10
+# Author: Lilkajt
+# discord: lilkajt#6121
 import time
 from discord import SyncWebhook
 from selenium import webdriver
@@ -8,24 +12,62 @@ import random
 import keyboard as BasicBoard
 import pandas as pd
 import os
+from win10toast import ToastNotifier
+toast = ToastNotifier()
 
-# 01.02.2023
-token = 'discordwebhook' # Put here your discord webhook to receive successfuly created accounts details.
+token = '' # Put here your discord webhook to receive successfuly created accounts details.
 webhook = SyncWebhook.from_url(token)
 url = "https://store.steampowered.com/join"
 region = "ARS" # region where you create accounts
-Limit = 10 #How many accounts you want to generate
+Limit = 1 #How many accounts you want to generate
 
 def random_string(length):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 def random_email():
     username = random_string(8)
-    domain = "yourdomain"
+    domain = "your-domain" #change to your domain
     return f"{username}@{domain}"
 
 def random_password(length):
     return random_string(length)
+
+def Notification(Type = "reCaptcha" or "email" or "name"):
+    while True:
+        if Type == "email":
+            key = "m"
+        elif Type == "reCaptcha":
+            key = "c"
+        else:
+            key = "n"
+        toast.show_toast(
+            f"Check {Type} Steam",
+            f"Press \"{key}\" to continue!",
+            duration = 10,
+            threaded = True,
+        )
+        if (toast.notification_active()):
+            if (BasicBoard.is_pressed(key)):
+                break
+
+def Name_check(driver, basic = None):
+    if (driver.find_element(By.ID, 'accountname_availability').text == "Not Available"):
+        Notification("name")
+        username = driver.find_element(By.XPATH, '//*[@id="accountname"]').text
+        return str(username)
+    else:
+        return str(basic)
+
+def ExcelFile(email, username,password):
+    data = {"Email": email, "Username": username, "Password": password}
+    df = pd.DataFrame(data, index=[0])
+    file_path = f"{os.path.dirname(os.path.abspath(__file__))}\\{region}_accounts.xlsx"
+
+    if not os.path.exists(file_path):
+        df.to_excel(file_path,sheet_name="Accounts",index=False)
+    else:
+        with pd.ExcelWriter(file_path,mode='a',engine='openpyxl', if_sheet_exists='overlay') as writer:
+            df.to_excel(writer,sheet_name="Accounts",startrow=writer.sheets["Accounts"].max_row,index=False, header=False)
 
 def SteamAccount():
     email = random_email()
@@ -41,16 +83,10 @@ def SteamAccount():
     for x in email: driver.find_element(By.XPATH, '//*[@id="reenter_email"]').send_keys(x), time.sleep(np.random.uniform(0,0.1))
     time.sleep(0.5)
     driver.find_element(By.XPATH, '//*[@id="i_agree_check"]').click()
-    while True:
-        print("Press \"c\" to continue! CAPTCHA!")
-        if (BasicBoard.is_pressed('c')):
-            break
+    Notification("reCaptcha")
     driver.find_element(By.XPATH, '//*[@id="createAccountButton"]').click()
     print("Creating Account - confirm mail")
-    while True:
-        print("Press \"m\" to continue! Mail!")
-        if (BasicBoard.is_pressed('m')):
-            break
+    Notification("email")
     time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="accountname"]').click()
     for x in username: driver.find_element(By.XPATH, '//*[@id="accountname"]').send_keys(x), time.sleep(np.random.uniform(0,0.1))
@@ -60,25 +96,14 @@ def SteamAccount():
     time.sleep(0.5)
     driver.find_element(By.XPATH, '//*[@id="reenter_password"]').click()
     for x in password: driver.find_element(By.XPATH, '//*[@id="reenter_password"]').send_keys(x), time.sleep(np.random.uniform(0,0.1))
+    username = Name_check(driver, basic=username)
     time.sleep(0.5)
     driver.find_element(By.XPATH, '//*[@id="createAccountButton"]').click()
-    time.sleep(2)
+    time.sleep(4)
     print("Account succesfuly created")
     ExcelFile(email,username,password)
-    webhook.send("Steam account created\nEmail: "+email+"\nUsername: "+username+"\nPassword: "+password)
+    webhook.send(f"Steam account created\nEmail: {email}\nUsername: {username}\nPassword: {password}")
     driver.close()
-
-def ExcelFile(email, username,password):
-    data = {"Email": email, "Username": username, "Password": password}
-    df = pd.DataFrame(data, index=[0])
-
-    file_path = f"yourfilepath{region}_accounts.xlsx" #input your file path
-    if not os.path.exists(file_path):
-        df.to_excel(file_path,sheet_name="Accounts",index=False)
-    else:
-        with pd.ExcelWriter(file_path,mode='a',engine='openpyxl', if_sheet_exists='overlay') as writer:
-            df.to_excel(writer,sheet_name="Accounts",startrow=writer.sheets["Accounts"].max_row,index=False, header=False)
-
 
 try:
     Count = 0
@@ -86,6 +111,6 @@ try:
         SteamAccount()
         time.sleep(1)
         Count +=1
-        print("Accounts generated: "+str(Count))
+        print(f"Accounts generated: {Count}")
 except Exception as err:
     print(err)
